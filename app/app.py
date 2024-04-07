3991a454d4bf3e7e612a37c06e70a21574267520
pedro-fs-garcia <pedrofsgarcia.pro@gmail.com>
2024-04-06 21:09:46 -0300

    inclusão do índice com bootstrap e mais correções

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route("/apostila/<name>")
 def find_page(name):
-    return render_template(f"{name}.html", page = name)
+    #arquivo json para guardar as páginas e ser usado para a construção do indice
+    apostila_paginas = json.load(open("./templates/apostila_paginas.json", encoding='utf-8'))
+    return render_template(f"{name}.html", page = name, paginas = apostila_paginas)
+
 
 
 if __name__ == "__main__":
