6196b7de32687b855d82ae1c7395bd2605620012
pedro-fs-garcia <pedrofsgarcia.pro@gmail.com>
2024-04-10 22:21:44 -0300

    pequenas correções

--- app/app.py
+++ app/app.py
@@ -4,10 +4,16 @@ import json
 app = Flask(__name__)
 
 @app.route("/apostila/<name>")
-def find_page(name):
+def find_apostila(name):
     #arquivo json para guardar as páginas e ser usado para a construção do indice
     apostila_paginas = json.load(open("./templates/apostila_paginas.json", encoding='utf-8'))
-    return render_template(f"{name}.html", page = name, paginas = apostila_paginas)
+    return render_template(f"{name}.html", paginas = apostila_paginas)
+
+
+@app.route("/<name>")
+def get_page(name):
+    apostila_paginas = json.load(open("./templates/apostila_paginas.json", encoding='utf-8'))
+    return render_template(f"{name}.html", paginas = apostila_paginas)
 
 
 
