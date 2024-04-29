d5741255c07699260b36c865ea5ae3d7706a9808
pedro-fs-garcia <pedrofsgarcia.pro@gmail.com>
2024-04-29 19:56:48 -0300

    organização da páginas da avaliação

--- app/app.py
+++ app/app.py
@@ -108,6 +108,7 @@ def resultado():
     return render_template("/quiz/resultado.html", acertos = acertos, erros = erros, porcentagem = porcentagem, respostas = respostas, questoes_erradas = questoes_erradas, correcao = correcao)
 
 
+# rota para avaliação e resultado da avaliação
 @app.route("/avaliacao", methods=["POST", "GET"])
 def avaliacao():
     apostila_paginas = arquivos.apostila_paginas
@@ -118,6 +119,8 @@ def avaliacao():
     if request.method == "POST":
         respostas_avaliacao = {}
         acertos = 0
+
+        nome_usuario = request.form["nome_usuario"].title()
 
         for i in range(1, len(perguntas)+1):
             respostas_avaliacao[i] = request.form.get(f"respostaquiz_assunto{i}")
@@ -131,26 +134,10 @@ def avaliacao():
         erros = len(perguntas) - acertos
         porcentagem = f"{(acertos/len(perguntas) * 100):.2f}%"
 
-        return render_template("resultado_avaliacao.html", acertos = acertos, erros = erros, porcentagem = porcentagem, respostas = respostas_avaliacao, questoes_erradas = questoes_erradas, correcao = correcao)
+        return render_template("/avaliacao/resultado_avaliacao.html", acertos = acertos, erros = erros, porcentagem = porcentagem, respostas = respostas_avaliacao, questoes_erradas = questoes_erradas, correcao = correcao, nome_usuario=nome_usuario, paginas = apostila_paginas)
 
+    return render_template("/avaliacao/avaliacao.html", perguntas = perguntas, apostile_paginas = apostila_paginas)
 
-    return render_template("avaliacao.html", perguntas = perguntas, apostile_paginas = apostila_paginas)
-
-
-# @app.route("/resultado_avaliacao", methods=["POST", "GET"])
-# def resultado_avaliacao():
-#     perguntas = arquivos.quiz_perguntas
-#     respostas_avaliacao = {}
-#     acertos = 0
-
-#     for i in range(1, len(perguntas)+1):
-#         respostas_avaliacao[i] = request.form.get(f"respostaquiz_assunto{i}")
-    
-#     for key, value in respostas_avaliacao.items():
-#         if value == perguntas[f"quiz_assunto{key}"][5]:
-#             acertos += 1
-
-#     return render_template("resultado_avaliacao.html", perguntas = perguntas, acertos = acertos)
 
 
 if __name__ == "__main__":
