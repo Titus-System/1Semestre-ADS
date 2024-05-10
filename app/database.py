bdf26f41eb749a351aa3af87c7b100f27367b488
Tiny-Mushroom <wasilewskiialice@gmail.com>
2024-05-10 14:28:04 -0300

    Fix database.py

--- app/database.py
+++ app/database.py
@@ -8,35 +8,35 @@ cur = con.cursor()
 #Operação para criar uma tabela caso nao houver
 res = cur.execute("SELECT name FROM sqlite_master WHERE name='registro'")
 if res.fetchone() is None:
-    cur.execute('''CREATE TABLE registro(
-        cpf varchar(11) NOT NULL UNIQUE,
-        nome varchar(100) NOT NULL,
-        nota tinyint(225) NOT NULL, 
-        feedback float(1),
-        comment varchar(500),
-        PRIMARY KEY (cpf)
-        )''')
+    cur.execute('''CREATE TABLE "registro" (
+	"cpf"	varchar(20) NOT NULL UNIQUE,
+	"nome"	varchar(100) NOT NULL,
+	"nota"	tinyint(225) NOT NULL,
+	"feedback"	float(1),
+	"comment"	varchar(500),
+	PRIMARY KEY("cpf")
+)''')
 
 #Inserir uma nota aou final da prova            
 def insertgrade(cpf, nome, nota):
     data = (cpf, )
-    res = cur.execute("SELECT FROM registro WHERE cpf='?'", data)
-    if res.fetchone() != None:
+    res = cur.execute("SELECT cpf FROM registro WHERE cpf=?", data)
+    if res.fetchone() is not None:
         data = (nota, cpf)
-        res = cur.execute("UPDATE registro SET nota='?' WHERE cpf='?'", data)
+        cur.execute("UPDATE registro SET nota=? WHERE cpf=?", data)
     else:
         data = (cpf, nome, nota)
-        res = cur.execute("INSERT INTO registro (cpf, nome, nota) VALUES(?, ?, ?)", data)
-        
+        cur.execute("INSERT INTO registro (cpf, nome, nota) VALUES(?, ?, ?)", data)
+
 #inserir um feedback ao final do curso
 def insertfeedback(cpf, feedback, comment):
     data = (cpf, )
-    res = cur.execute("SELECT FROM registro WHERE cpf='?'", cpf) #ve se a pessoa fez a prova
+    res = cur.execute("SELECT cpf FROM registro WHERE cpf=?", cpf) #ve se a pessoa fez a prova
     if res.fetchone() == None:
         return False #retorna o bool False se nao foi feita a prova
     else:
         data = (feedback, cpf)
-        res = cur.execute("UPDATE registro SET feedback='?' WHERE cpf='?'", data)
-        if comment:
+        res = cur.execute("UPDATE registro SET feedback=? WHERE cpf=?", data)
+        if comment: #ve se o comentario nao é uma string vazia ou None
             data = (comment, cpf)
-            res = cur.execute("UPDATE registro SET comment='?' WHERE cpf='?'", data)
+            cur.execute("UPDATE registro SET comment=? WHERE cpf=?", data)
