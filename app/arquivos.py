866c89a1d9ac20bc91d3136316ae1d043fb62e35
pedro-fs-garcia <pedrofsgarcia.pro@gmail.com>
2024-06-05 21:44:18 -0300

    correçõs paginas v ou f e associação

--- app/arquivos.py
+++ app/arquivos.py
@@ -7,6 +7,3 @@ quiz_perguntas = json.load(open("./static/quiz_perguntas.json", encoding='utf-8'
 perguntas_prova = json.load(open("./static/perguntas_prova.json", encoding='utf-8'))
 
 erro_assunto = json.load(open("./static/erro_assunto.json", encoding='utf-8'))
-
-modal_sem_resposta = "https://code.jquery.com/jquery-3.5.1.min.js"
-
